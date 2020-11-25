50a061e813d723d9d922bcf3121a4758b43b2d31
nishantkabra123 <nishantkabra123@gmail.com>
2020-11-26 02:10:52 +0530

    Added: validated first name

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -1,3 +1,14 @@
 #!/bin/bash -x
+shopt -s extglob
+read -p "Enter First Name : " name
 
-echo welcome
+pat='^[A-Z]{1}[a-z]{2,}$'
+
+if [[ $name =~ $pat ]]
+then
+    echo "Yes"
+else
+    echo "No"
+fi
+
+
